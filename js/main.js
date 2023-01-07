aa797f26d918f62b5d2f7a1c4b1e7b92602fff9f
tryloggit <nmaafk0510@gmail.com>
2023-01-07 22:38:00 +0200

    fix button click logic

--- js/main.js
+++ js/main.js
@@ -2,19 +2,21 @@ let counter=0;
 function oneClickButton(element){
     counter++;
     element.innerHTML="Click me for plus: ";
-    element.style.background="green"
     console.log((counter));
+    changeMainButton();
 }
 function oneClickButtonminus(element){
     counter--;
     element.innerHTML="Click me for minus: ";
-    element.style.background="green"
     console.log((counter));
+    changeMainButton();
     }
-function oneClickButtonsum(element){
-    element.style.background="green";
-    if (counter>=10){(element.style.background="red")};
-    if (counter<=10){(element.style.background="blue")+(element.style.color="white")};
-    if (counter<=0){(element.innerHTML="Sum: "+counter)};//это юзлес строка я знаю, это была попытка сделать другое.
-    element.innerHTML="Sum: "+counter
+function changeMainButton(){
+    const button = document.getElementById("centerbutton")
+    button.innerHTML=`Sum: ${counter}`
+    button.style.background="green";
+    if (counter>=10){
+        (button.style.background="red")};
+    if (counter<=-10){
+        (button.style.background="blue")+(button.style.color="white")};
 }
