10c69941932e34493c00d28325dd953abce7e877
tryloggit <nmaafk0510@gmail.com>
2023-01-07 21:45:44 +0200

    updating main.js file

--- js/main.js
+++ js/main.js
@@ -15,6 +15,6 @@ function oneClickButtonsum(element){
     element.style.background="green";
     if (counter>=10){(element.style.background="red")};
     if (counter<=10){(element.style.background="blue")+(element.style.color="white")};
-    if (counter<=0){(element.innerHTML="Sum: "+counter)};
+    if (counter<=0){(element.innerHTML="Sum: "+counter)};//это юзлес строка я знаю, это была попытка сделать другое.
     element.innerHTML="Sum: "+counter
 }
