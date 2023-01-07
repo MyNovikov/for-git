a6e798cd3c26ffef4208434ddffc4c894bfc7e9e
tryloggit <nmaafk0510@gmail.com>
2023-01-07 21:04:50 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+let counter=0;
+function oneClickButton(element){
+    counter++;
+    element.innerHTML="Click me for plus: ";
+    element.style.background="green"
+    console.log((counter));
+}
+function oneClickButtonminus(element){
+    counter--;
+    element.innerHTML="Click me for minus: ";
+    element.style.background="green"
+    console.log((counter));
+    }
+function oneClickButtonsum(element){
+    element.style.background="green";
+    if (counter>=10){(element.style.background="red")};
+    if (counter<=10){(element.style.background="blue")+(element.style.color="white")};
+    if (counter<=0){(element.innerHTML="Sum: "+counter)};
+    element.innerHTML="Sum: "+counter
+}
